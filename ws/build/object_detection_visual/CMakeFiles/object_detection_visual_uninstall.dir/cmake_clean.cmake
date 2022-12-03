8aede58b2d2a7db2ec239fc3f4bed2c2c115f568
Radu Popescu <radu.popescu@omnirobotic.com>
2022-12-02 21:00:43 -0500

    clean

--- ws/build/object_detection_visual/CMakeFiles/object_detection_visual_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/object_detection_visual_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/object_detection_visual_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
